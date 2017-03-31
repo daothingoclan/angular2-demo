60fb034e60e19a125811326c7f0ddd20486aba63
Lan Dao <ngoclan141091@gmail.com>
2017-03-31 16:46:14 +0700

    no message

--- src/modules/journal/aptJournal/aptJournals.ts
+++ src/modules/journal/aptJournal/aptJournals.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import aptJournalHelper from "../_share/helpers/aptJournalHelper";
 import appConst from "../_share/const/apiBaseUrl";
 
-@Component({   
+@Component({
     templateUrl: "src/modules/journal/aptJournal/aptJournals.html",
     inputs: ['aptJournals']
 })
@@ -12,17 +12,17 @@ export class AptJournals {
     public aptJournals: Array<any>;
     public dateFormat: string = appConst.formatDate;
 
-    constructor() {
-        this.onPatientIdChanged("95");
-    }
+    // constructor() {
+    //     this.onPatientIdChanged("95");
+    // }
 
-    public onPatientIdChanged(patientId: string) {
+    public onPatientIdChanged(patientId: any) {
         this.patientId = patientId;
         this.loadAptJournals(patientId);
     }
 
     private loadAptJournals(patientId: string) {
-        let iaptJournalService = window.ioc.resolve("IAptJournalService");  
+        let iaptJournalService = window.ioc.resolve("IAptJournalService");
         iaptJournalService.getAptsByPatient(patientId).subscribe(
             (aptJournals: Array<any>) => {
                 this.loadAptJournalDone(aptJournals);
@@ -37,12 +37,11 @@ export class AptJournals {
             item.html = aptJournalHelper.convertDataToHtml(item.journal.data);
         });
         self.aptJournals = aptJournals;
-       // console.log(self.aptJournals);
     }
 
-    public  getSize(item: any, index:number) {
+    public getSize(item: any, index: number) {
         let size = 300;
-         //var element = document.elem('all_items_container');
+        //var element = document.elem('all_items_container');
         // element.innerHTML = item.html;
         // size = element.clientHeight;
         return size;
